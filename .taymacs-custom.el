e02739ee9ae4cdb7ae88f2d357b1af7bda54a9fd
taylor <hardy.taylor.j@gmail.com>
2020-07-26 16:53:15 -0400

    remove bloat, fix some spacing

--- .taymacs-custom.el
+++ .taymacs-custom.el
@@ -11,17 +11,35 @@
     ("0230fd6c26a0805f34a634fc34de284e414982db2e31c696638f521201919f83" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "922b4d7f68af5017f980398284229c81bb94ac17b9f3f23082dd0a4b2d0c7666" default)))
  '(erc-autojoin-channels-alist
    (quote
-    (("localhost" "#kulupupitokipona#1" "#EsperantoAmeriko#1")
-     ("freenode.net" "#guix" "##latinitas" "##latin" "#anime" "#emacs" "#hurd" "#guix"))))
+    (("localhost" "#VergeCurrency|News" "&bitlbee" "#kulupupitokipona#1" "#EsperantoAmeriko#1")
+     ("freenode.net" "#fsf" "#searx" "#guix" "##latinitas" "##latin" "#emacs" "#hurd" "#guix"))))
  '(erc-autojoin-timing (quote ident))
  '(erc-modules
    (quote
     (completion log notifications hl-nicks netsplit fill button match track readonly networks ring autojoin noncommands irccontrols move-to-prompt stamp menu list)))
- '(eww-search-prefix "https://duckduckgo.com/lite/?q=")
+ '(erc-track-exclude
+   (quote
+    ("##latinitas" "##latin" "#EsperantoAmeriko#1" "#kulupupitokipona#1")))
+ '(erc-track-exclude-types (quote ("JOIN" "KICK" "NICK" "PART" "333" "353")))
+ '(eww-search-prefix "http://nosearch.nogroup.group/searx/?q=")
  '(gnus-asynchronous t)
  '(gnus-use-full-window nil)
  '(helm-completion-style (quote helm))
  '(ivy-mode t)
+ '(org-capture-templates
+   (quote
+    (("k" "koan" entry
+      (file+headline "~/notes/cap/koans.org" "unsorted")
+      "** %? %i %T ")
+     ("d" "todo" entry
+      (file+headline "~/notes/cap/todo.org" "unsorted")
+      "** TODO %? %i %T ")
+     ("p" "projects" entry
+      (file+headline "~/notes/cap/projects.org" "unsorted")
+      "** %? %i %T "))))
+ '(org-datetree-add-timestamp (quote inactive))
+ '(org-default-notes-file "~/notes/notes.org")
+ '(org-directory "~/notes")
  '(org-file-apps
    (quote
     ((auto-mode . emacs)
@@ -32,7 +50,7 @@
  '(org-journal-dir "~/notes/journal/")
  '(package-selected-packages
    (quote
-    (language-detection ivy modus-vivendi-theme counsel nnhackernews auctex acutex elpher moe-theme color-theme-modern cider haskell-mode forge prettier-js org-journal web-mode key-chord evil doom-modeline diff-hl aggressive-indent ace-window helm-ag vue-mode salaire-mode doom-themes editorconfig telephone-line eyeliner spaceline-all-the-icons tabbar neotree js2-refactor company-tern tern ergoemacs-mode dracula-theme golden-ratio-scroll-screen slime-company slime company-jedi zzz-to-char rainbow-delimiters avy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package)))
+    (eloud language-detection ivy modus-vivendi-theme counsel nnhackernews auctex acutex elpher moe-theme color-theme-modern cider haskell-mode forge prettier-js org-journal web-mode key-chord evil doom-modeline diff-hl aggressive-indent ace-window helm-ag vue-mode salaire-mode doom-themes editorconfig telephone-line eyeliner spaceline-all-the-icons tabbar neotree js2-refactor company-tern tern ergoemacs-mode dracula-theme golden-ratio-scroll-screen slime-company slime company-jedi zzz-to-char rainbow-delimiters avy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package)))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8")
  '(safe-local-variable-values
@@ -43,6 +61,7 @@
             (concat
              (locate-dominating-file default-directory ".dir-locals.el")
              "node_modules/.bin/"))))))
+ '(semantic-mode t)
  '(shr-use-fonts nil)
  '(tabbar-separator (quote (0.5)))
  '(tramp-default-method "scp"))
