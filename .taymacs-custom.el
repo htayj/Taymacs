8e32cfda482a1d866fedd49ea5cea9fdbc744a1d
Taylor Hardy <taylor.hardy@codemettle.com>
2022-12-30 04:52:06 -0500

    laptop changes

--- .taymacs-custom.el
+++ .taymacs-custom.el
@@ -6,10 +6,12 @@
  '(ansi-color-names-vector
    ["#303030" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(browse-url-secondary-browser-function 'eaf-open-browser)
+ '(create-lockfiles nil)
  '(custom-file "~/.emacs.d/.taymacs-custom.el")
  '(custom-safe-themes
    '("0230fd6c26a0805f34a634fc34de284e414982db2e31c696638f521201919f83" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "922b4d7f68af5017f980398284229c81bb94ac17b9f3f23082dd0a4b2d0c7666" default))
  '(dired-mode-hook nil)
+ '(eaf-find-alternate-file-in-dired t t)
  '(eaf-wm-focus-fix-wms
    '("i3" "/usr/share/xsessions/i3" "qtile" "/usr/share/xsessions/qtile" "emacs" "wmctrl -m"))
  '(elfeed-enclosure-default-dir "/mnt/Ext4Storage/ytdl/elfeed")
@@ -24,10 +26,11 @@
  '(erc-track-exclude
    '("##latinitas" "##latin" "#EsperantoAmeriko#1" "#kulupupitokipona#1"))
  '(erc-track-exclude-types '("JOIN" "KICK" "NICK" "PART" "333" "353"))
- '(evil-default-state 'emacs)
  '(evil-insert-state-modes nil)
  '(evil-motion-state-modes nil)
- '(eww-search-prefix "https://www.duckduckgo.com/lite?q=")
+ '(evil-search-module 'evil-search)
+ '(evil-want-keybinding t)
+ '(eww-search-prefix "https://duckduckgo.com/lite/?q=")
  '(forge-alist
    '(("github.com" "api.github.com" "github.com" forge-github-repository)
      ("git.codemettle.com" "git.codemettle.com/api/v4" "git.codemettle.com" forge-gitlab-repository)
@@ -42,13 +45,14 @@
      ("repo.or.cz" nil "repo.or.cz" forge-repoorcz-repository)
      ("git.suckless.org" nil "git.suckless.org" forge-stagit-repository)
      ("git.sr.ht" nil "git.sr.ht" forge-srht-repository)))
- '(global-prettier-mode t)
+ '(global-prettier-mode nil)
  '(gnus-asynchronous t)
  '(gnus-use-full-window nil)
  '(helm-autoresize-mode nil)
  '(helm-completion-style 'helm)
+ '(helm-minibuffer-history-key "M-p")
  '(ivy-mode t)
- '(lsp-metals-treeview-show-when-views-received nil)
+ '(lsp-metals-treeview-show-when-views-received nil t)
  '(org-agenda-files '("~/notes/notes.org"))
  '(org-capture-templates
    '(("k" "koan" entry
@@ -95,6 +99,8 @@ During:%K")))
      ("\\.mm\\'" . default)
      ("\\.x?html?\\'" . "firefox %s")
      ("\\.pdf\\'" . default)))
+ '(org-journal-date-format "%A, %d %B %Y")
+ '(org-journal-dir "~/notes/journal/")
  '(org-structure-template-alist
    '(("el" "emacs-lisp")
      ("a" . "export ascii")
@@ -108,11 +114,12 @@ During:%K")))
      ("s" . "src")
      ("v" . "verse")))
  '(package-selected-packages
-   '(prettier tide lsp-ui company-lsp lsp-metals lsp-mode sbt-mode scala-mode evil-surround evil-collection shrface ace-link ivy-rich ivy-prescient counsel-projectile all-the-icons-dired all-the-icons emms-player-mpv elfeed-org elfeed paredit eloud language-detection ivy modus-vivendi-theme counsel nnhackernews auctex acutex elpher moe-theme color-theme-modern cider haskell-mode forge prettier-js org-journal web-mode key-chord evil doom-modeline diff-hl aggressive-indent ace-window helm-ag vue-mode salaire-mode doom-themes editorconfig telephone-line eyeliner spaceline-all-the-icons tabbar neotree js2-refactor company-tern tern ergoemacs-mode dracula-theme golden-ratio-scroll-screen slime-company slime company-jedi zzz-to-char rainbow-delimiters avy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package))
+   '(eloud nnhackernews elfeed-org elfeed emms-player-mpv elpher auctex tide company-lsp lsp-ui lsp-metals lsp-mode sbt-mode scala-mode prettier paredit ace-link ivy-prescient counsel-projectile all-the-icons-dired language-detection modus-vivendi-theme evil-surround evil-collection moe-theme color-theme-modern cider haskell-mode forge prettier-js org-journal web-mode key-chord evil doom-modeline diff-hl aggressive-indent ace-window helm-ag vue-mode salaire-mode doom-themes editorconfig telephone-line eyeliner spaceline-all-the-icons tabbar neotree js2-refactor company-tern tern ergoemacs-mode dracula-theme golden-ratio-scroll-screen slime-company slime company-jedi zzz-to-char rainbow-delimiters avy ivy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8")
  '(prettier-js-args
    '("--print-width" "120" "--arrow-parens" "avoid" "--trailing-comma" "all" "--find-config-path"))
+ '(projectile-completion-system nil)
  '(projectile-ignored-projects nil)
  '(projectile-indexing-method 'hybrid)
  '(projectile-project-root-files '(".projectile"))
